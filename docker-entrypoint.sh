9bf8e50b853615a033aa34742f0e52c1cd57efde
Benoît Suttor <benoit.suttor@imio.be>
2015-07-22 14:23:10 +0200

    Update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /usr/bin/python manage.py syncdb                   # Apply database migrations
-/usr/bin/python manage.py makemigrations
+/usr/bin/python manage.py migrate
 # Start Gunicorn processes
 echo Starting python manage.py runserver on port 8000.
 exec /usr/bin/python manage.py runserver 0.0.0.0:8000
